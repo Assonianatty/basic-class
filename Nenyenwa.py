32b1a74dbad10aad81a31212c1bcd0958c09fc32
nenyenwa <chinenyeabuone1@gmail.com>
2025-08-11 08:27:11 -0700

    Nenyenwa

---
+++ Nenyenwa.py
@@ -0,0 +1,11 @@
+print("Am Nenyenwa Assumpta Abuone from akwaeze (umuideke) in anaocha LGA Anambra state")
+print("born on the 10th of october (10/10) in Enugu state") 
+print("am black tall slim in identity")
+print("am certified in healthcare, class of 2025 graduate in accounting")
+print("am a christian in religion, catholic chorister who fears GOD")
+print("am a lover of christ")
+print("i love sowing, braiding hair and editinng videos")
+print("find more fun in cooking as well")
+print("in a family of 5 children, am the first daughter of the first daughter")\
+
+
--- hello.py
+++ hello.py
@@ -1,2 +1,5 @@
-print("nenyenwa\nis\nmy\nname\nim\ndoing\npython")
-print("hello")
+print("hello nenye")
+print("hello majesty")
+print("my name is nenye, i am a student of aptech")
+
+
