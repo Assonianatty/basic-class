82d848e00dc414c1922b981c651dca0cb59ca5e2
nenyenwa <chinenyeabuone1@gmail.com>
2025-08-18 05:51:57 -0700

    my

--- assignment.py
+++ assignment.py
@@ -17,3 +17,27 @@ print(num1 + num2)
 print(num1 - num2)
 print(num1 / num2)
 print(num1 * num2)
+
+
+first number : 20
+second number : 10
+what is your operation? : sum
+this is the sum of two numbers: 30
+
+using %
+first  number: 20
+second number :10
+what is your operation?: quotient
+this is the quotient of two numbers: 2.0
+
+
+first number: 20
+second number: 10
+what is your operation?: difference
+this is the difference of two numbers: 10
+
+
+first number: 20
+second number:10
+what is your operation?: multiplication
+this is the multiplication of two numbers: 200 
--- str.py
+++ str.py
@@ -7,3 +7,25 @@ print(f"this is my name {name}")
 txt = "we are the so_called \"vikings\"from the north."
 print(txt)
 # the boys are "swagging" 
+
+
+
+
+
+
+name = "pascal"
+print(f"normal")
+upper_name="SANCTUS"
+print(upper_name)
+
+
+text = "this is a string of text"
+text_b = "debbbie"
+text_count = text.count("T")
+print(text_count)
+#Add add
+
+
+
+
+
